b7885dcc8602b2ad78e98d9c3755ab224ccdd6c4
Yuri Garrido <y.garrido.yuri@gmail.com>
2022-07-12 21:13:36 -0300

    edit connection

--- src/connection.ts
+++ src/connection.ts
@@ -4,8 +4,7 @@ import mongoose from 'mongoose';
 const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop';
 
 const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGO_URI
-    || MONGO_DB_URL,
+  mongoDatabaseURI = process.env.MONGO_URI || MONGO_DB_URL,
 ) => mongoose.connect(mongoDatabaseURI);
 
 export default connectToDatabase;
